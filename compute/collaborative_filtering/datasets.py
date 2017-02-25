d0d99c4236898fcd07748a928d60fb7f0cfc1f72
manas-ticketfly <manas.hardas@ticketfly.com>
2017-02-24 17:30:43 -0800

    Remove some functions

--- compute/collaborative_filtering/datasets.py
+++ compute/collaborative_filtering/datasets.py
@@ -1,6 +1,4 @@
-import time
 import pandas
-from django.utils import timezone
 import numpy as np
 from scipy.sparse import coo_matrix
 from sklearn.preprocessing import LabelEncoder
@@ -14,42 +12,36 @@ user_artists = pandas.read_csv(
 # Userids are not incremented by 1 starting from 0. Therefore we map the userids
 # and attractionids to an index starting from 0 and incremented by.
 
-def generate_user_index():
-    le = LabelEncoder()
-    le.fit(user_artists['user_dim_id'])
-    user_index = le.transform(user_artists['user_dim_id'])
-    return user_index
+le = LabelEncoder()
+le.fit(user_artists['user_dim_id'])
+user_index = le.transform(user_artists['user_dim_id'])
 
-def generate_user_index_mapping():
-    user_index_mapping = pandas.DataFrame({
-        'user_dim_id': user_artists['user_dim_id'],
-        'user_email_address': user_artists['user_email_address'],
-        'serial': user_index
-    })
-    user_index_mapping.set_index('serial')
-    return user_index_mapping
+user_index_mapping = pandas.DataFrame({
+    'user_dim_id': user_artists['user_dim_id'],
+    'user_email_address': user_artists['user_email_address'],
+    'serial': user_index
+})
+user_index_mapping.set_index('serial')
+writeable_users = user_index_mapping.drop_duplicates(subset=['user_dim_id'])
 
-def generate_attraction_index():
-    le = LabelEncoder()
-    le.fit(user_artists['attraction_attraction_id'])
-    attraction_index = le.transform(user_artists['attraction_attraction_id'])
-    return attraction_index
+le = LabelEncoder()
+le.fit(user_artists['attraction_attraction_id'])
+attraction_index = le.transform(user_artists['attraction_attraction_id'])
 
-def generate_attraction_index_mapping():
-    attraction_index_mapping = pandas.DataFrame({
-        'serial': attraction_index,
-        'attraction_id': user_artists['attraction_attraction_id'],
-        'attraction_name': user_artists['attraction_attraction_name']
-    })
-    attraction_index_mapping.set_index('serial')
-    return attraction_index_mapping
+attraction_index_mapping = pandas.DataFrame({
+    'serial': attraction_index,
+    'attraction_id': user_artists['attraction_attraction_id'],
+    'attraction_name': user_artists['attraction_attraction_name']
+})
+attraction_index_mapping.set_index('serial')
+writeable_attractions = attraction_index_mapping.drop_duplicates(
+    subset=['attraction_id'])
 
 
 # generate the user-attraction   matrix
 mat = coo_matrix((
     np.ones(user_index.shape[0]),       # weights
-    (generate_user_index(), generate_attraction_index())      # adjacency list
+    (user_index, attraction_index)      # adjacency list
 ))
-df = mat.toarray()
-useritemdf = pandas.DataFrame(df)
-itemuserdf = pandas.DataFrame(df.transpose())
+useritemdf = mat.toarray()
+itemuserdf = df.transpose()
