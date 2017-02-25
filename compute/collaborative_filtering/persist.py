72f87aafa0e9c43bf3ccd0d4dce70971f375b56d
manas-ticketfly <manas.hardas@ticketfly.com>
2017-02-24 17:30:57 -0800

    Persist db functions

--- compute/collaborative_filtering/persist.py
+++ compute/collaborative_filtering/persist.py
@@ -1,14 +1,10 @@
-import time
-import pandas
 from django.utils import timezone
-import numpy as np
-from scipy.sparse import coo_matrix
-from sklearn import preprocessing
 
-from datasets import generate_user_index_mapping
-from datasets import generate_attraction_index_mapping
+from datasets import writeable_users
+from datasets import writeable_attractions
 from datasets import useritemdf
 from datasets import itemuserdf
+from util import get_id_for_serial
 
 from sklearn.decomposition import TruncatedSVD
 from sklearn.neighbors import LSHForest
@@ -19,11 +15,6 @@ from recommend.models import SimilarAttractions
 
 
 def persist_user_data_to_db():
-    user_index_mapping = generate_user_index_mapping()
-    writeable_users = user_index_mapping.drop_duplicates(
-        subset=['user_dim_id']
-    )
-
     for i, u in writeable_users.iterrows():
         try:
             u = User(
@@ -36,11 +27,6 @@ def persist_user_data_to_db():
 
 
 def persist_attractions_data_to_db():
-    attraction_index_mapping = generate_attraction_index_mapping()
-    writeable_attractions = attraction_index_mapping.drop_duplicates(
-        subset=['attraction_id']
-    )
-
     for i, e in writeable_attractions.iterrows():
         try:
             e = Attraction(
@@ -50,7 +36,6 @@ def persist_attractions_data_to_db():
             e.save()
         except Exception as ex:
             print(ex)
-
 
 
 def persist_user_similarities_to_db():
@@ -63,7 +48,7 @@ def persist_user_similarities_to_db():
     # persist user similarities to db
     K=20        # query for K neighbors
     k=10        # return k neighbors
-    for i in useritemdf.index:
+    for i in useritemdf.shape[0]:
         distance, indices = user_item_model.kneighbors(
             red_dim_useritemdf[i].reshape(1, -1), n_neighbors=K
         )
@@ -71,10 +56,10 @@ def persist_user_similarities_to_db():
         for j in range(k):
             if i != indices[0][j]:
                 s = SimilarUsers(
-                    user_dim_id=get_dim_id_for_serial(
-                        serial=int(i), dim_id_type='user'),
-                    similar_user_dim_id=get_dim_id_for_serial(
-                        serial=int(indices[0][j]), dim_id_type='user'),
+                    user_dim_id=get_id_for_serial(
+                        serial=int(i), id_type='user'),
+                    similar_user_dim_id=get_id_for_serial(
+                        serial=int(indices[0][j]), id_type='user'),
                     similarity=weights[0][j],
                     ts=timezone.now()
                 )
@@ -91,7 +76,7 @@ def persist_attraction_similarities_to_db():
     # persist attractions similarities to db
     K=20        # query for K neighbors
     k=10        # return k neighbors
-    for i in itemuserdf.index:
+    for i in itemuserdf.shape[0]:
         distance, indices = item_user_model.kneighbors(
             red_dim_itemuserdf[i].reshape(1, -1), n_neighbors=K
         )
@@ -99,10 +84,10 @@ def persist_attraction_similarities_to_db():
         for j in range(k):
             if i != indices[0][j]:
                 e = SimilarAttractions(
-                    attraction_id=get_dim_id_for_serial(
-                        serial=int(i), dim_id_type='attraction'),
-                    similar_attraction_id=get_dim_id_for_serial(
-                        serial=int(indices[0][j]), dim_id_type='attraction'),
+                    attraction_id=get_id_for_serial(
+                        serial=int(i), id_type='attraction'),
+                    similar_attraction_id=get_id_for_serial(
+                        serial=int(indices[0][j]), id_type='attraction'),
                     similarity=weights[0][j],
                     ts=timezone.now()
                 )
