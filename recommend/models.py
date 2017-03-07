91914284a950afe572483be24c0b2984ecb031d6
manas-ticketfly <manas.hardas@ticketfly.com>
2017-03-07 10:15:10 -0800

    Add app_id, replace user_dim_id with user_id

--- recommend/models.py
+++ recommend/models.py
@@ -3,18 +3,22 @@ from django.db import models
 
 
 class User(models.Model):
-    user_dim_id = models.IntegerField()
+    app_id = models.IntegerField(default=000)
+    user_id = models.IntegerField()
     email = models.CharField(max_length=200)
+    first = models.CharField(max_length=100)
+    last = models.CharField(max_length=100)
 
 
 class Attraction(models.Model):
+    app_id = models.IntegerField(default=000)
     attraction_id = models.IntegerField()
     attraction_name = models.CharField(max_length=2000)
 
 
 class SimilarUsers(models.Model):
-    user_dim_id = models.IntegerField()
-    similar_user_dim_id = models.IntegerField()
+    user_id = models.IntegerField()
+    similar_user_id = models.IntegerField()
     similarity = models.FloatField()
     ts = models.DateTimeField()
 
@@ -27,10 +31,10 @@ class SimilarAttractions(models.Model):
 
 #
 # class UserAttractionHistory(models.Model):
-#     user_dim_id = models.IntegerField()
+#     user_id = models.IntegerField()
 #     attraction_id = models.IntegerField()
 #
 #
 # class AttractionUserHistory(models.Model):
 #     attraction_id = models.IntegerField()
-#     user_dim_id = models.IntegerField()
+#     user_id = models.IntegerField()
